58634e2a39302c066fac18ccaac8a5d52575f039
pr-oleksandr <prystash.oleksandr@icloud.com>
2026-02-07 08:30:02 +0000

    Deploying to gh-pages from @ pr-oleksandr/artists_hub_project@4084eca98f1de7d63b47703383bf1da949243c6e 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import{a as c,i as m}from"./assets/vendor-CK1Rzdhu.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const a of e)if(a.type==="childList")for(const n of a.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const a={};return e.integrity&&(a.integrity=e.integrity),e.referrerPolicy&&(a.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?a.credentials="include":e.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function o(e){if(e.ep)return;e.ep=!0;const a=r(e);fetch(e.href,a)}})();c.defaults.baseURL="https://sound-wave.b.goit.study/api";const p=document.querySelector(".loader"),g=s=>{const t=Math.floor(s/6e4),r=Math.floor(s%6e4/1e3);return`${t}:${r<10?"0":""}${r}`};async function v(s){if(!s){m.error({title:"Error",message:"Artist ID is missing."});return}try{p.classList.remove("is-hidden");const t=await c.get(`/artists/${s}`),o=(await c.get(`/artists/${s}/albums`)).data.albumsList||[],e=t.data;y(e,o)}catch(t){console.error("Error fetching artist details:",t),m.error({title:"Error",message:"Failed to load artist details."})}finally{p.classList.add("is-hidden")}}function y(s,t){const r=document.querySelector(".artist-modal");r&&r.remove();const o=document.createElement("div");o.className="artist-modal",o.innerHTML=`
+import{a as c,i as m}from"./assets/vendor-CK1Rzdhu.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const a of e)if(a.type==="childList")for(const i of a.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const a={};return e.integrity&&(a.integrity=e.integrity),e.referrerPolicy&&(a.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?a.credentials="include":e.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function r(e){if(e.ep)return;e.ep=!0;const a=o(e);fetch(e.href,a)}})();const f=document.querySelector(".loader");function b(){f.classList.remove("is-hidden")}function h(){f.classList.add("is-hidden")}c.defaults.baseURL="https://sound-wave.b.goit.study/api";const p=document.querySelector(".loader"),v=s=>{const t=Math.floor(s/6e4),o=Math.floor(s%6e4/1e3);return`${t}:${o<10?"0":""}${o}`};async function y(s){if(!s){m.error({title:"Error",message:"Artist ID is missing."});return}try{p.classList.remove("is-hidden");const t=await c.get(`/artists/${s}`),r=(await c.get(`/artists/${s}/albums`)).data.albumsList||[],e=t.data;$(e,r)}catch(t){console.error("Error fetching artist details:",t),m.error({title:"Error",message:"Failed to load artist details."})}finally{p.classList.add("is-hidden")}}function $(s,t){const o=document.querySelector(".artist-modal");o&&o.remove();const r=document.createElement("div");r.className="artist-modal",r.innerHTML=`
         <div class="modal-content">
             <button type="button" class="modal-close-btn" aria-label="Close">x</button>
             <h2 class="m-a-name">${s.strArtist}</h2>
@@ -26,34 +26,34 @@ import{a as c,i as m}from"./assets/vendor-CK1Rzdhu.js";(function(){const t=docum
                 </div>
                 <div class="genres-container">
                     <p class="m-a-topic">Genres</p>
-                    <p class="m-a-genres-container">${s.genres.map(n=>`<span class="m-a-genre">${n}</span>`).join("")}</p>
+                    <p class="m-a-genres-container">${s.genres.map(i=>`<span class="m-a-genre">${i}</span>`).join("")}</p>
                 </div>
             </div>
             <ul class="m-a-albums">
             <p class="m-a-albums-topic">Albums</p>
-            ${t.map(n=>`
+            ${t.map(i=>`
                 <li class="m-a-album-item">
-                <span class="m-a-album-name">${n.strAlbum}</span>
+                <span class="m-a-album-name">${i.strAlbum}</span>
                 <ul class="m-a-track-names">
-                ${n.tracks.map(i=>`
+                ${i.tracks.map(n=>`
                         <div class="m-a-track-row">
-                            <span class="m-a-track-name">${i.strTrack}</span>
-                            <span class="m-a-track-duration">${g(i.intDuration)}</span>
-                            <a class="m-a-track-link" href="${i.strTrackThumb||"#"}"  target="_blank">Y</a>
+                            <span class="m-a-track-name">${n.strTrack}</span>
+                            <span class="m-a-track-duration">${v(n.intDuration)}</span>
+                            <a class="m-a-track-link" href="${n.strTrackThumb||"#"}"  target="_blank">Y</a>
                         </div>`).join("")}
                 </ul>
                 </li>`).join("")}
             </ul>
         </div>  
-    `,document.body.appendChild(o),o.querySelector(".modal-close-btn").addEventListener("click",()=>{o.remove()});const a=n=>{n.key==="Escape"&&(o.remove(),document.removeEventListener("keydown",a))};document.addEventListener("keydown",a)}c.defaults.baseURL="https://sound-wave.b.goit.study/api";const u=document.querySelector(".artists-list"),l=document.querySelector(".load-more-btn"),f=document.querySelector(".loader");let d=1;const h=8;u.addEventListener("click",function(s){const t=s.target.closest(".learn-more-btn");if(!t)return;const r=t.dataset.id;v(r)});async function $(s){return(await c.get("/artists",{params:{page:s,limit:h,sortName:"asc"}})).data}function L(s,t=20){if(!s)return"";const r=s.indexOf("."),o=s.split(" ");if(r!==-1){const e=s.substring(0,r+1);if(e.split(" ").length<=t)return e}return o.length>t?o.slice(0,t).join(" ")+"...":s}function w(s,t=!1){const r=s.map(({_id:o,strArtist:e,strArtistThumb:a,genres:n,strBiographyEN:i})=>`
+    `,document.body.appendChild(r),r.querySelector(".modal-close-btn").addEventListener("click",()=>{r.remove()});const a=i=>{i.key==="Escape"&&(r.remove(),document.removeEventListener("keydown",a))};document.addEventListener("keydown",a)}c.defaults.baseURL="https://sound-wave.b.goit.study/api";const u=document.querySelector(".artists-list"),l=document.querySelector(".load-more-btn");let d=1;const L=8;g();l.addEventListener("click",()=>{d+=1,g()});u.addEventListener("click",function(s){const t=s.target.closest(".learn-more-btn");if(!t)return;const o=t.dataset.id;y(o)});async function w(s){return(await c.get("/artists",{params:{page:s,limit:L}})).data}function k(s,t=20){if(!s)return"";const o=s.indexOf("."),r=s.split(" ");if(o!==-1){const e=s.substring(0,o+1);if(e.split(" ").length<=t)return e}return r.length>t?r.slice(0,t).join(" ")+"...":s}function E(s,t=!1){const o=s.map(({_id:r,strArtist:e,strArtistThumb:a,genres:i,strBiographyEN:n})=>`
         <li class="artist-item">
             <img src="${a}" alt="${e}" class="artist-img">
             <div class="artist-info">
-                <p class="genres">${n.join(", ")}</p>
+                <p class="genres">${i.join(", ")}</p>
                 <h3 class="artist-name">${e}</h3>
-                <p class="artist-bio">${L(i,20)}</p> 
-                <button type="button" class="learn-more-btn" data-id="${o}">Learn more</button>
+                <p class="artist-bio">${k(n,20)}</p> 
+                <button type="button" class="learn-more-btn" data-id="${r}">Learn more</button>
             </div>
         </li>
-    `).join("");t?u.insertAdjacentHTML("beforeend",r):u.innerHTML=r}async function b(){try{f.classList.remove("is-hidden"),l.classList.add("is-hidden");const s=await $(d);w(s.artists,d>1),s.page>=s.totalPages?(l.classList.add("is-hidden"),d>1&&m.info({message:"Оппа! Це кінесь, більше нікого немає."})):l.classList.remove("is-hidden")}catch{m.error({message:"Лишенько, щось пішло не так"})}finally{f.classList.add("is-hidden")}}b();l.addEventListener("click",()=>{d+=1,b()});
+    `).join("");t?u.insertAdjacentHTML("beforeend",o):u.innerHTML=o}async function g(){try{b(),l.classList.add("is-hidden");const s=await w(d);E(s.artists,d>1),s.page>=s.totalPages?(l.classList.add("is-hidden"),d>1&&m.info({position:"topRight",message:"Ouch! That is all, folks! No more artists to show."})):l.classList.remove("is-hidden")}catch{m.error({position:"topRight",message:"Welcome to Ukraine! Blackout again? Or the server is resting..."})}finally{h()}}
 //# sourceMappingURL=index.js.map
