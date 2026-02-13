390b245c32516d2cc8e237720f412e7ac3bd977e
Нікіта <gwstudiogw48090@gmail.com>
2026-02-13 19:56:45 +0200

    custom pagination

--- src/js/artists.js
+++ src/js/artists.js
@@ -181,10 +181,27 @@ function initPagination(totalItems) {
   pagination = new Pagination(container, {
     totalItems: totalItems,
     itemsPerPage: limit,
-    visiblePages: 5,
+    visiblePages: 3,
     page: currentPage,
     centerAlign: true,
     usageStatistics: false,
+    template: {
+      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      currentPage:
+        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      moveButton:
+        '<a href="#" class="tui-page-btn tui-{{type}} ">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</a>',
+      disabledMoveButton:
+        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</span>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-more">' +
+        '<span class="tui-ico-more">...</span>' +
+        '</a>',
+    },
   });
 
   pagination.on('afterMove', event => {
